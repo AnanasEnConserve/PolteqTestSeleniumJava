7e41aab258e850651dcb63949f50bcffc349d2a9
Arnout van Heereveld <arnoutvanh@gmail.com>
2022-03-08 15:29:34 +0100

    added soft assertions

---
+++ src/test/java/steps/GeneralTest.java
@@ -0,0 +1,10 @@
+package steps;
+
+import org.assertj.core.api.SoftAssertions;
+
+public class GeneralTest {
+
+    public static final SoftAssertions softAssertions = new SoftAssertions();
+
+
+}
--- src/test/java/steps/TestFeature.java
+++ src/test/java/steps/TestFeature.java
@@ -5,14 +5,15 @@ import abstraction.PageObject;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
-public class TestFeature {
+public class TestFeature extends GeneralTest{
 
     private HomePage homePage;
 
     @Given("I am on the homepage")
     public void iAmOnTheHomepage(){
         homePage = PageObject.getHomePage();
-        homePage.isHeaderLoaded();
+        softAssertions.assertThat(homePage.isHeaderLoaded()).as("Did not see header but expected to").isTrue();
+        softAssertions.assertAll();
     }
 
     @Then("I should be able to click the button")
