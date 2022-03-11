8ce30a7eceea735e0f78e48345245dce1e7c9214
Arnout van Heereveld <arnoutvanh@gmail.com>
2022-03-11 17:09:19 +0100

    added html xml and json reports

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
-        glue = {"steps"}
+        glue = {"steps"},
+        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports/Cucumber.html"}
 )
 public class TestRunner {
 
