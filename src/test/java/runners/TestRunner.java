f3fa841d7ed0cc0152359df5ef2fc8a1899f88e4
Arnout van Heereveld <arnoutvanh@gmail.com>
2022-03-11 18:36:14 +0100

    created more fancy html reports (NOTE: These are generated on verify step)

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,9 +8,11 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         glue = {"steps"},
-        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+        plugin = {
+                "pretty", "json:target/cucumber-reports/Cucumber.json",
                 "junit:target/cucumber-reports/Cucumber.xml",
-                "html:target/cucumber-reports/Cucumber.html"}
+                "html:target/cucumber-reports/Cucumber.html",
+                "json:target/cucumber-JVM-reports/cucumber.json"}
 )
 public class TestRunner {
 
