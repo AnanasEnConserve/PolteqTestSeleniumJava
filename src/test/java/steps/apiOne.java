4753c82c210b5f4d9141ca68bf224e9e538034f8
Arnout van Heereveld <arnoutvanh@gmail.com>
2022-03-30 11:59:55 +0200

    prevented api tests from using unnecessary selenium steps

--- src/test/java/steps/apiOne.java
+++ src/test/java/steps/apiOne.java
@@ -4,23 +4,24 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
 public class apiOne extends GeneralTest {
 
     @Given("I log in with valid credentials")
-    public ValidatableResponse attemptAccessWithValidCredentials(){
+    public Response attemptAccessWithValidCredentials(){
         return RestAssured.given().baseUri(environment.getApiUrl() + "/api")
                 .when()
                 .formParam("username", apiKey)
                 .and().formParam("password", "")
-                .and().post()
-                .then().log().status();
+                .and().post();
+                //.then().log().status();
     }
 
     @Then("I should get a {int} response")
     public void iShouldGetA200Response(int expectedResponse){
-        softAssertions.assertThat(attemptAccessWithValidCredentials()).as("Server response different than expected").isEqualTo(expectedResponse);
+        softAssertions.assertThat(attemptAccessWithValidCredentials().getStatusCode()).as("Server response different than expected").isEqualTo(expectedResponse);
         softAssertions.assertAll();
     }
 
