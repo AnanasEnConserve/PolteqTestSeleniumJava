6c488bfff38053f18083df48b05260601bc72141
Arnout van Heereveld <arnoutvanh@gmail.com>
2022-03-30 15:11:54 +0200

    added invalid credential test

--- src/test/java/steps/apiOne.java
+++ src/test/java/steps/apiOne.java
@@ -17,9 +17,24 @@ public class apiOne extends GeneralTest {
                 .then().extract().response();
     }
 
+    @Given("I log in with invalid credentials")
+    public Response attemptAccessWithInvalidCredentials(){
+        return RestAssured.given().auth()
+                .basic("blablabla", "")
+                .when()
+                .get(environment.getApiUrl() + "/api")
+                .then().extract().response();
+    }
+
     @Then("I should get a {int} response")
     public void iShouldGetA200Response(int expectedResponse){
         softAssertions.assertThat(attemptAccessWithValidCredentials().getStatusCode()).as("Server response different than expected").isEqualTo(expectedResponse);
+        softAssertions.assertAll();
+    }
+
+    @Then("I should get rejected and receive a {int} response")
+    public void iShouldGetA401Response(int expectedResponse){
+        softAssertions.assertThat(attemptAccessWithInvalidCredentials().getStatusCode()).as("Server response different than expected").isEqualTo(expectedResponse);
         softAssertions.assertAll();
     }
 
